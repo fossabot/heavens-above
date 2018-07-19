6b6231822e76704bf5db8ad915085b393734df8e
Zhu Zijia <littlepiggy03@hotmail.com>
2018-07-19 23:15:35 +0800

    chore: edit commitlint rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,1 @@
-module.exports = {
-    extends: [
-        '@commitlint/config-conventional'
-    ],
-    rules: {
-        "subject-case": [ 2, "always", [ "sentence-case" ] ]
-    }
-};
+module.exports = { extends: [ '@commitlint/config-conventional' ] };
