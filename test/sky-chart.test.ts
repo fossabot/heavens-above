c595791c303cb6d7ca8d5a328d3ea3be214dab66
Zhu Zijia <littlepiggy03@hotmail.com>
2018-07-20 10:28:27 +0800

    test: fix test problem

--- test/sky-chart.test.ts
+++ test/sky-chart.test.ts
@@ -5,18 +5,18 @@ import * as assert from "assert";
 
 import HeavensAbove from "../lib";
 
-// tslint:disable no-function-expression
+// tslint:disable no-function-expression mocha-no-side-effect-code
+
+const HA = new HeavensAbove();
 
 describe("Sky Chart", function (): void {
 
     it("Should be able to get sky chart URL (first time)", async function (): Promise<void> {
-        const HA = new HeavensAbove();
         const url = await HA.getSkyChartURL();
         assert(typeof url === "string" && url.length > 0);
     });
 
     it("Should be able to get sky chart URL (not first time)", async function (): Promise<void> {
-        const HA = new HeavensAbove();
         const url = await HA.getSkyChartURL({
             time: new Date("2018-07-01T18:00:00+0800"),
             language: "en",
--- test/sun.test.ts
+++ test/sun.test.ts
@@ -5,18 +5,18 @@ import * as assert from "assert";
 
 import HeavensAbove from "../lib";
 
-// tslint:disable no-function-expression
+// tslint:disable no-function-expression mocha-no-side-effect-code
+
+const HA = new HeavensAbove();
 
 describe("Sun", function (): void {
 
     it("Should be able to get sun info (first time)", async function (): Promise<void> {
-        const HA = new HeavensAbove();
         const data = await HA.getSunInfo();
         assert(typeof data === "object");
     });
 
     it("Should be able to get sun info (not first time)", async function (): Promise<void> {
-        const HA = new HeavensAbove();
         const data = await HA.getSunInfo({
             time: new Date("2018-07-01T18:00:00+0800")
         });
